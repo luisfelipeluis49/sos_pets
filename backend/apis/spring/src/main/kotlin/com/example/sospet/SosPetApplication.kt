9d19cb9283bfaf56ea6de9b7d69958a575c56aa7
luisfelipeluis49 <luisfelipeluis49@gmail.com>
2022-08-01 03:31:59 -0300

    Added a Spring Web API project

---
+++ backend/apis/spring/src/main/kotlin/com/example/sospet/SosPetApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sospet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SosPetApplication
+
+fun main(args: Array<String>) {
+	runApplication<SosPetApplication>(*args)
+}
